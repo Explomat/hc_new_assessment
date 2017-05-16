c3e65a65f106ebd408ed20da6ca77052f71d9203
Explomat <cabehok@inbox.ru>
2017-05-16 22:43:06 +0300

    change dev config

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,8 @@ module.exports = {
         host: '0.0.0.0',
         port: 8080,
         contentBase: './dist',
-        hot: true
+        hot: true,
+        disableHostCheck: true
     },
 
     plugins: [
